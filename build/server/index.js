c3eeae45f357f151517cb227e470cbcf6cc65ed8
uslessmty <597697242@qq.com>
2024-04-28 02:28:09 +0800

    feat: 服务端打包

---
+++ build/server/index.js
@@ -0,0 +1,17 @@
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const koa_1 = require("koa");
+const app = new koa_1.default();
+app.use((ctx) => __awaiter(void 0, void 0, void 0, function* () {
+    ctx.body = 'Hello World';
+}));
+app.listen(3000);
